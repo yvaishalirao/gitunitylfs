ae89f3f7d5d178913424d43c0c122a35bfa97cca
Duke0fCodes <deek5h17h.c@gmail.com>
2024-05-07 22:18:25 +0530

    all leds and game over

--- Assets/Scripts/button.cs
+++ Assets/Scripts/button.cs
@@ -3,16 +3,32 @@ using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using JetBrains.Annotations;
 using UnityEngine;
+using TMPro;
+using UnityEngine.Video;
+using Unity.VisualScripting;
 
 public class button : MonoBehaviour
 {
-    public GameObject led1;
+    public GameObject[] led;
     public GameObject bomb;
     public boomtimer timer;
+    [SerializeField] GameObject defutext;
+    [SerializeField] GameObject bmb;
+    [SerializeField] GameObject timtext;
+    [SerializeField] GameObject gotext;
+
+    [SerializeField] GameObject scr;
+    public VideoPlayer vp;
+
+
     private float Remaining;
     // Start is called before the first frame update
     void Start()
     {
+        led[0].GetComponent<SpriteRenderer>().color = Color.red;
+        led[1].GetComponent<SpriteRenderer>().color = Color.red;
+        led[2].GetComponent<SpriteRenderer>().color = Color.red;
+        led[3].GetComponent<SpriteRenderer>().color = Color.red;
 
     }
 
@@ -20,14 +36,32 @@ public class button : MonoBehaviour
     void Update()
     {
        Remaining = Mathf.FloorToInt(timer.remTime)%60;
+        if((led[0].GetComponent<SpriteRenderer>().color == Color.green) && (led[1].GetComponent<SpriteRenderer>().color == Color.green) &&(led[2].GetComponent<SpriteRenderer>().color == Color.green) &&(led[3].GetComponent<SpriteRenderer>().color == Color.green))
+        {
+            timtext.SetActive(false);
+            defutext.SetActive(true);
+        }
     
     }
     void OnMouseDown()
-    {
+    {  
         if(Remaining%10==7)
-        led1.GetComponent<SpriteRenderer>().color = Color.green;
-        else
-        bomb.SetActive(false);
+        led[0].GetComponent<SpriteRenderer>().color = Color.green;
+        else if(Remaining%10==5)
+        led[1].GetComponent<SpriteRenderer>().color = Color.green;
+        else if(Remaining%10==2)
+        led[2].GetComponent<SpriteRenderer>().color = Color.green;
+        else if(Remaining%10==0)
+        led[3].GetComponent<SpriteRenderer>().color = Color.green;
+        else 
+        {
+        bmb.GetComponent<SpriteRenderer>().sprite=null;
+        timtext.SetActive(false);
+        gotext.SetActive(true);
+        scr.SetActive(true);
+        vp.Play();
+        }
+
     }
 }
 
